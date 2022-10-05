20ebb92622a88b8c4244fb94334cb197f0396d86
praks27 <gamingmaster456789@gmail.com>
2022-10-05 19:40:34 +0700

    update Seeder & Faker

--- database/seeders/MajorSeeder.php
+++ database/seeders/MajorSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Major;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class MajorSeeder extends Seeder
 {
@@ -14,6 +15,14 @@ class MajorSeeder extends Seeder
      */
     public function run()
     {
+        Major::truncate();
         //
+        $majors = ["informatika","marketing","VCDM","akuntansi"];
+        foreach ($majors as $key => $major) {
+            Major::create([
+                "name" => $major,
+                "description" => "pembelajaran tentang $major"
+            ]);
+        }
     }
 }
--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Factory;
+use App\Models\Major;
+use App\Models\Student;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class StudentSeeder extends Seeder
 {
@@ -14,6 +17,28 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Student::truncate();
+
+        $faker = Factory::create('id_ID');
+        $majors =Major::get();
+
+        for($i=0 ; $i<100 ; $i++) {
+            $gender = ( $i % 2 ) ? 'male' : 'female';
+            Student::create([
+                    'name' => $faker->name($gender),
+                    'date_birth' => '2015-01-01',
+                    'gender' => $gender,
+                    'address' => $faker->address,
+                    'major_id' => $majors->random()->id
+            ]);
+        }
+        //untuk  membuat seeder 1 1 atau manual
+        // Student::create([
+        //     "name" => "Putra",
+        //     "date_birth" => "2015-01-01",
+        //     "gender" => "Male",
+        //     "address" => "jl.selayar kapal",
+        //     "major_id" => 1
+        // ]);
     }
 }
